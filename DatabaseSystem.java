3226ca2fc82bf49bda307a782dc4c5b363eadbc6
milkmanmatty <dreamer@live.com.au>
2014-10-08 19:22:07 +1000

    Added to DatabaseSystem.java
    Fixed indentation and added some basic getter methods.

--- DatabaseSystem.java
+++ DatabaseSystem.java
@@ -24,7 +24,7 @@ public class DatabaseSystem {
 		/* 
 		true if customer found
 		false if customer not found.
-		*/
+		 */
 		for (int i = 0; i < customers.size(); i++) {
 			Customer cus = customers.get(i);
 			if (cus.getCusID() == cusID) {
@@ -44,7 +44,7 @@ public class DatabaseSystem {
 		/*
 		returns null if no customer with id found.
 		returns first Customer with id otherwise.
-		*/
+		 */
 
 		for (int i = 0; i < customers.size(); i++) {
 			Customer cus = customers.get(i);
@@ -64,7 +64,7 @@ public class DatabaseSystem {
 		returns cusID for name if customer is found.
 		NOTE: name must be entered as it exists in the database.
 		NOTE: character case in names is ignored.
-		*/
+		 */
 
 		int numberFound = 0;
 		int lastID = 0;
@@ -91,34 +91,34 @@ public class DatabaseSystem {
 		returns cusID for name if customer is found.
 		NOTE: name must be entered as it exists in the database.
 		NOTE: character case in names is ignored.
-		*/
+		 */
 
 		int numberFound = 0;
 		int lastID = 0;
 		for (int i = 0; i < customers.size(); i++) {
 			Customer cus = customers.get(i);
 			if ((cus.getName().equalsIgnoreCase(name)) &&
-				cus.getPhone().equalsIgnoreCase(phone)) {
+					cus.getPhone().equalsIgnoreCase(phone)) {
 				numberFound++;
-			lastID = cus.getCusID();
+				lastID = cus.getCusID();
+			}
+		}
+		if (numberFound == 1) {
+			return lastID;
+		} else {
+			return numberFound;
 		}
 	}
-	if (numberFound == 1) {
-		return lastID;
-	} else {
-		return numberFound;
+
+	public static void addMenuItem(String name, String cost) {
+		MenuItem menuItem = new MenuItem(name, cost);
 	}
-}
 
-public static void addMenuItem(String name, String cost) {
-	MenuItem menuItem = new MenuItem(name, cost);
-}
-
-public static boolean addMenuItem(int menuItemID, String name, String cost) {
+	public static boolean addMenuItem(int menuItemID, String name, String cost) {
 		/* 
 		returns true if item ID doesn't already exist and item added successfully
 		returns false if item ID already exists and item was not added.
-		*/
+		 */
 
 		for (int i = 0; i < customers.size(); i++) {
 			MenuItem menuItem = menuItems.get(i);
@@ -135,7 +135,7 @@ public static boolean addMenuItem(int menuItemID, String name, String cost) {
 		/*
 		Returns true if item does exist within the menuItems Array
 		else returns false.
-		*/
+		 */
 
 		for (int i = 0; i < menuItems.size(); i++) {
 			MenuItem menuItem = menuItems.get(i);
@@ -144,6 +144,10 @@ public static boolean addMenuItem(int menuItemID, String name, String cost) {
 			}
 		}
 		return false;
+	}
+
+	protected static ArrayList<MenuItem> getMenuItems(){
+		return menuItems;
 	}
 
 	public static BigDecimal getMenuItemCost(int id) {
